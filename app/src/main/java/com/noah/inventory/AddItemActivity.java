641e4a373fc7a00d6e746db73204d0db891dc191
Noah Bar <noahbar@gmail.com>
2018-06-18 11:10:32 +0200

    activity_add_item.xml done.

--- app/src/main/java/com/noah/inventory/AddItemActivity.java
+++ app/src/main/java/com/noah/inventory/AddItemActivity.java
@@ -1,13 +1,14 @@
 package com.noah.inventory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class AddItemActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_item);
     }
+
 }
