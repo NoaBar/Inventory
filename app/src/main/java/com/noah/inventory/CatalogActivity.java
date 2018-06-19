f48976e145a3fadfd3b47d64f65491b99142052b
Noah Bar <noahbar@gmail.com>
2018-06-19 14:19:41 +0200

    CatalogActivity.java hopefully done.

--- app/src/main/java/com/noah/inventory/CatalogActivity.java
+++ app/src/main/java/com/noah/inventory/CatalogActivity.java
@@ -1,29 +1,124 @@
 package com.noah.inventory;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import com.noah.inventory.Data.ItemContract.ItemEntry;
+import com.noah.inventory.Data.ItemDbHelper;
 
 public class CatalogActivity extends AppCompatActivity {
+
+    /** Database helper to provide access to the database */
+    private ItemDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
+        // Setup FAB (floating action button) to open AddItemActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(CatalogActivity.this, AddItemActivity.class);
+                startActivity(intent);
             }
         });
+
+        mDbHelper = new ItemDbHelper(this);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayDatabaseInfo();
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the items database.
+     */
+    private void displayDatabaseInfo() {
+
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                ItemEntry._ID,
+                ItemEntry.COLUMN_ITEM_NAME,
+                ItemEntry.COLUMN_ITEM_PRICE,
+                ItemEntry.COLUMN_ITEM_QUANTITY,
+                ItemEntry.COLUMN_ITEM_CATEGORY,
+                ItemEntry.COLUMN_ITEM_SUPPLIER_NAME,
+                ItemEntry.COLUMN_ITEM_SUPPLIER_PHONE_NUMBER
+        };
+
+        Cursor cursor = db.query(
+                ItemEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        TextView displayView = (TextView) findViewById(R.id.text_view_items);
+
+        try {
+            // Create a header in the Text View that looks like this:
+            //
+            // The items table contains <number of rows in Cursor> items.
+            // _id - name - price - quantity - category - supplier name - supplier phone
+            //
+            // In the while loop below, iterate through the rows of the cursor and display
+            // the information from each column in this order.
+            displayView.setText("The items table contains " + cursor.getCount() + " pets.\n\n");
+            displayView.append(ItemEntry._ID + " - " +
+                    ItemEntry.COLUMN_ITEM_NAME + " - " +
+                    ItemEntry.COLUMN_ITEM_PRICE + " - " +
+                    ItemEntry.COLUMN_ITEM_QUANTITY + " - " +
+                    ItemEntry.COLUMN_ITEM_CATEGORY + " - " +
+                    ItemEntry.COLUMN_ITEM_SUPPLIER_NAME + " - "+
+                    ItemEntry.COLUMN_ITEM_SUPPLIER_PHONE_NUMBER + "\n");
+
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(ItemEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_QUANTITY);
+            int categoryColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_CATEGORY);
+            int supplierNameColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_SUPPLIER_NAME);
+            int supplierPhoneColumnIndex = cursor.getColumnIndex(ItemEntry.COLUMN_ITEM_SUPPLIER_PHONE_NUMBER);
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                int currentPrice = cursor.getInt(priceColumnIndex);
+                int currentQuantity = cursor.getInt(quantityColumnIndex);
+                int currentCategory = cursor.getInt(categoryColumnIndex);
+                String currentSupplierName = cursor.getString(supplierNameColumnIndex);
+                String currentSupplierPhone = cursor.getString(supplierPhoneColumnIndex);
+
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentName + " - " +
+                        currentPrice + " - " +
+                        currentQuantity + " - " +
+                        currentCategory + " - " +
+                        currentSupplierName + " - " +
+                        currentSupplierPhone));
+            }
+        } finally {
+            cursor.close();
+        }
+    }
 }
