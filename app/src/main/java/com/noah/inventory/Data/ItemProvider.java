bcc41a6f5bcde35dcd94f6eabbdf7ca8b6045f12
Noah Bar <noahbar@gmail.com>
2018-07-06 12:34:35 +0200

    improved list_item.xml

---
+++ app/src/main/java/com/noah/inventory/Data/ItemProvider.java
@@ -0,0 +1,15 @@
+package com.noah.inventory.Data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
+
+
+public class ItemProvider extends ContentProvider {
+
+}
